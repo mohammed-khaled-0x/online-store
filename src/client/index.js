1fedd49409ff3410428444800f68dad748771a43
Mohammed <mohammed.khaled0x@outlook.com>
2021-02-10 01:12:43 +0200

    Edit Header and add svg in html file

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,8 @@
+//import styles
 import './styles/main.scss';
 import './styles/header.scss';
+
+//import javascript
+import './js/animation'
+import './js/image-src'
+import './js/cursor-click'
---
+++ src/client/js/image-src.js
@@ -0,0 +1,4 @@
+import bodyBackground from '../media/background.png';
+
+const body = document.body;
+body.style.backgroundImage = `url(${bodyBackground})`
