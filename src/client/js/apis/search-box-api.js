e1063923f4d3129b72e654c8b10c3253d2f7d714
Mohammed <mohammed.khaled0x@outlook.com>
2021-02-22 02:16:40 +0200

    add api file and done of search box result and api

--- src/client/js/apis/search-box-api.js
+++ src/client/js/apis/search-box-api.js
@@ -2,7 +2,30 @@ const searchBox = document.getElementById('search_box');
 
 searchBox.onkeyup = async () => {
     const searchBoxValue = document.getElementById('search_box').value;
+    const searchResultPlaceholder = document.getElementById('search_result_placeholder');
+    searchResultPlaceholder.innerText = 'please wait...';
+
+    
+
     if(searchBoxValue.length > 3) {
+
+        const searchBox = document.getElementById('search_box');
+        const searchResultContainer = document.getElementById('search_result_container');
+        const resultContainer = document.getElementsByClassName('result-container')[0];
+        searchBox.style.borderBottomLeftRadius = 0;
+        searchBox.style.borderBottomRightRadius = 0;
+        searchResultContainer.style.display = 'flex';
+
+        setTimeout( () => {
+            searchResultContainer.style.height = '10em';
+            resultContainer.style.display = 'flex';
+        }, 500 );
+
+        setTimeout( () => {
+            resultContainer.style.opacity = 1;
+            resultContainer.style.top = 0;
+        }, 1250)
+
         await fetch(`https://mystore9.herokuapp.com/products/auto-suggestion/${searchBoxValue}/`)
         .then(response => {
             if(response.statusText !== 'ok' || response.status !== 404) {
@@ -13,7 +36,47 @@ searchBox.onkeyup = async () => {
             }
         })
         .then(response => {
-            console.log(response)
+
+            //check if there any result
+            const resultOfSearch = document.getElementById('result_of_search');
+
+            if(resultOfSearch.hasChildNodes()) {
+                //searchResult.removeChild(document.querySelector('#result_of_search'))
+                resultOfSearch.innerHTML = '';
+            }
+
+            for(let i of response) {
+                let itemOfSearch = document.createElement('li');
+                itemOfSearch.innerHTML = i;
+                resultOfSearch.appendChild(itemOfSearch);
+            }
+
+            searchResultPlaceholder.style.opacity = 0;
+            const searchResult = document.getElementById('search_result');
+
+            setTimeout( () => {
+                resultOfSearch.style.display = 'flex';
+            }, 1000)
+            
+            setTimeout( () => {
+                searchResultPlaceholder.style.display = 'none';
+                resultOfSearch.style.opacity = 1;
+            }, 1100)
+
         });
+    } else if(searchBoxValue.length === 0) {
+        const resultOfSearch = document.getElementById('result_of_search');
+        resultOfSearch.style.opacity = 0;
+        setTimeout( () => {
+            resultOfSearch.style.display = 'none';
+        }, 1000)
+
+        searchResultPlaceholder.innerText = 'If you want to search for anything, look here, everything you want is waiting for you now, then what are you waiting for?'
+        setTimeout( () => {
+            searchResultPlaceholder.style.display = 'inline';
+        },1000)
+        setTimeout( () => {
+            searchResultPlaceholder.style.opacity = 1;
+        }, 1050)
     }
 };
--- src/client/js/serach-box-drop-list.js
+++ src/client/js/serach-box-drop-list.js
@@ -1,10 +1,11 @@
 const searchBox = document.getElementById('search_box');
 const searchResultContainer = document.getElementById('search_result_container');
-const resultHistory = document.getElementsByClassName('result-container')[0];
+const resultContainer = document.getElementsByClassName('result-container')[0];
 
-//get current target
 window.onclick = (e) => {
-    clearTimeout();
+
+    //check if client click on search box to open drop list.
+    //make sure that the drop list still open while click on it.
     let checkSearchBox, checkDropList;
     for(let i of e.path) {
         if(i.id === 'search_box') {
@@ -23,16 +24,31 @@ window.onclick = (e) => {
         searchBox.style.borderBottomLeftRadius = 0;
         searchBox.style.borderBottomRightRadius = 0;
         searchResultContainer.style.display = 'flex';
+
         setTimeout( () => {
             searchResultContainer.style.height = '10em';
-        }, 750 );
+            resultContainer.style.display = 'flex';
+        }, 500 );
+
+        setTimeout( () => {
+            resultContainer.style.opacity = 1;
+            resultContainer.style.top = 0;
+        }, 1250)
     } else {
-        searchResultContainer.style.height = 0;
+        resultContainer.style.opacity = 0;
+        resultContainer.style.top = '-2em';
+        var hideDropList = setTimeout( () => {
+            searchResultContainer.style.height = 0;
+        }, 500)
+
         setTimeout( () => {
             searchBox.style.borderBottomLeftRadius = '1em';
             searchBox.style.borderBottomRightRadius = '1em';
+        }, 1200 );
+
+        setTimeout( () => {
             searchResultContainer.style.display = 'none';
-        }, 755 );
+            resultContainer.style.display = 'none';
+        }, 1500 );
     }
-    clearTimeout();
 };
